fbb128d9269a6223e8150108a3d3e0dfb97cdaa5
ArtemYarmush <grywagrywa@gmail.com>
2023-04-16 18:26:45 +0300

    hw-08-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,53 +4,22 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const galleryList = document.querySelector('.gallery');
-
-const galleryMarkup = galleryItemCreate(galleryItems);
-//console.log(galleryMarkup);
-
-function galleryItemCreate(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-               <a class="gallery__link" href="${original}">
-                <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="${original}"
-                    alt="${description}"
-                />
-                </a>
-                </div>`;
-    })
-    .join('');
-}
-
-galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
-//console.log(galleryList);
-
-galleryList.addEventListener('click', onGalleryListClick);
-
-function onGalleryListClick(event) {
-  event.preventDefault();
-
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const modalImg = event.target.dataset.source;
-  //console.log(modalImg);
-  const instance = basicLightbox.create(`
-		<img width="1400" height="900" src="${modalImg}">
-	`);
-  instance.show();
-}
-
-window.addEventListener('keydown', event => {
-  if (event.code === 'Escape') {
-    instance.close();
-    window.removeEventListener('keydown');
-  }
-});
-
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+galleryList.style.listStyle = 'none';
+const markup = galleryItems
+  .map(
+    item =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+            </a>
+        </li>`
+  )
+  .join('');
+
+console.log(markup);
+galleryList.innerHTML = markup;
+
+let lightbox = new SimpleLightbox('.gallery a', {});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener(
+  'input',
+  throttle(e => {
+    const prevState = JSON.parse(localStorage.getItem('feedback-form-state'));
+    const inputEl = e.target;
+
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify({
+        ...prevState,
+        [inputEl.getAttribute('name')]: inputEl.value,
+      })
+    );
+  }, 500)
+);
+
+const inputEl = document.querySelector('.feedback-form input');
+inputEl.value =
+  JSON.parse(localStorage.getItem('feedback-form-state'))?.email || '';
+const messageEl = document.querySelector('.feedback-form textarea');
+messageEl.value =
+  JSON.parse(localStorage.getItem('feedback-form-state'))?.message || '';
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  console.log({ email: data.get('email'), message: data.get('message') });
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+});
